b338e7db76c874db3f7a73b5a5f6427dffd8f749
Friedemann Ang <friedemann.ang@gmail.com>
2019-02-25 14:56:59 +0800

    Changed logging of geocode functions to estimate completion time. Bugfixes.

--- api_tools.py
+++ api_tools.py
@@ -1,4 +1,5 @@
-from datatime import datetime, timedelta
+from datetime import datetime, timedelta
+import json
 import logging
 logging.getLogger().setLevel(logging.INFO)
 import pandas as pd
@@ -16,9 +17,12 @@ def google_geocode(address_list, cred):
     url = 'https://maps.googleapis.com/maps/api/geocode/json'
     result = []
         
-    unused_limit = 25000
+    LIMIT = 25000
+    unused_limit = LIMIT
     period = timedelta(hours=24)
-    start_time = datetime.now()
+    limit_start_time = datetime.now()
+    first_pass = True
+    est_passes = len(address_list) / LIMIT
     
     for address in address_list:
         answer = {
@@ -47,12 +51,14 @@ def google_geocode(address_list, cred):
         
         unused_limit -= 1
         if unused_limit < 1:
-            elapsed_period = datetime.now() - start_time
+            elapsed_period = datetime.now() - limit_start_time
+            est_time = datetime.now() + timedelta(days=(est_passes))
             remaining_period = period - elapsed_period
             remaining_seconds = remaining_period.total_seconds()
-            if remaining_seconds > 0:
+            if remaining_seconds > 0 and first_pass == True:
                 logging.info('Reached free limit for today in ' + str(elapsed_period // 3600) + ' hours, ' + str((elapsed_period % 3600) // 60) + ' min.')
-                logging.info('Resuming in ' + str(remaining_seconds // 3600) + ' hours, ' + str((remaining_seconds % 3600) // 60) + ' min.')
+                logging.info('Estimated completion at ' + str(est_time.isoformat()[:19]) + ' local time.')
+#                logging.info('Resuming in ' + str(remaining_seconds // 3600) + ' hours, ' + str((remaining_seconds % 3600) // 60) + ' min.')
                 sleep(remaining_seconds)
             unused_limit = 25000
             start_time = datetime.now()
@@ -71,10 +77,12 @@ def onemap_geocode(address_list):
     params = {'searchVal': None, 'returnGeom': 'Y', 'getAddrDetails':'Y', 'pageNum':'1'}
     result = []
     
-    LIMIT = 250    
+    LIMIT = 250
     unused_limit = LIMIT
     period = timedelta(seconds=60)
     limit_start_time = datetime.now()
+    first_pass = True
+    est_passes = len(address_list) / LIMIT
     
     for address in address_list:
         answer = {
@@ -103,11 +111,14 @@ def onemap_geocode(address_list):
         unused_limit -= 1
         if unused_limit < 1:
             elapsed_period = datetime.now() - limit_start_time
+            est_time = datetime.now() + timedelta(seconds=(60 * est_passes))
             remaining_period = period - elapsed_period
             remaining_seconds = remaining_period.total_seconds()
-            if remaining_seconds > 0:
-                logging.info('Reached free limit for this minute in ' + str(elapsed_period.total_seconds()) + 's.')
-                logging.info('Resuming in ' + str(remaining_seconds) + 's.')
+            if remaining_seconds > 0 and first_pass == True:
+                logging.info('Reached limit for first minute in ' + str(elapsed_period.total_seconds()) + 's.')
+                logging.info('Estimated completion at ' + str(est_time.isoformat()[:19]) + ' local time.')
+#                logging.info('Resuming in ' + str(remaining_seconds) + 's.')
+                first_pass = False
                 sleep(remaining_seconds)
             unused_limit = LIMIT
             limit_start_time = datetime.now()
